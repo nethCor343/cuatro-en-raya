e002cf1efe88a25b5686074a1c4faea2597927a5
Neth Corrales <nethcordev343@gmail.com>
2026-04-15 18:50:52 -0400

    feat: integrar logica principal de Partida y enum ResutadoTurno

---
+++ cuatro-en-raya/src/main/java/com/nethcorp/cuatroenraya/model/Partida.java
@@ -0,0 +1,60 @@
+package com.nethcorp.cuatroenraya.model;
+
+public class Partida {
+    private final Tablero tablero;
+    private final GestorTurnos turnos;
+    private final ReglasJuego reglas;
+    private boolean finalizado;
+
+    public Partida(int filas, int columnas) {
+        this.tablero = new Tablero(filas, columnas);
+        this.turnos = new GestorTurnos();
+        this.reglas = new ReglasJuego();
+        this.finalizado = false;
+    }
+
+    public ResultadoTurno jugar(int indiceColumna) {
+        if (finalizado) {
+        	return ResultadoTurno.JUEGO_TERMINADO;
+        }
+
+        EstadoCelda jugadorActual = turnos.obtenerTurnoActual();
+        int filaColocada = tablero.dejarCaerFicha(indiceColumna, jugadorActual);
+
+        if (filaColocada == -1) {
+            return ResultadoTurno.MOVIMIENTO_INVALIDO;
+        }
+
+        if (reglas.verificarVictoria(tablero, filaColocada, indiceColumna)) {
+            finalizado = true;
+            return ResultadoTurno.VICTORIA;
+        }
+
+        if (reglas.verificarEmpate(tablero)) {
+            finalizado = true;
+            return ResultadoTurno.EMPATE;
+        }
+
+        turnos.siguienteTurno();
+        return ResultadoTurno.EN_CURSO;
+    }
+
+    public Tablero obtenerTablero() {
+    	return tablero;
+    }
+    
+    public EstadoCelda obtenerTurnoActual() {
+    	return turnos.obtenerTurnoActual();
+    }
+    
+    public EstadoCelda obtenerGanador() {
+    	return reglas.obtenerGanador();
+    	
+    }
+    
+    public boolean verificarFinalJuego() {
+    	return finalizado;
+    	
+    }
+    
+}
---
+++ cuatro-en-raya/src/main/java/com/nethcorp/cuatroenraya/model/ResultadoTurno.java
@@ -0,0 +1,5 @@
+package com.nethcorp.cuatroenraya.model;
+
+public enum ResultadoTurno {
+	EN_CURSO, VICTORIA, EMPATE, MOVIMIENTO_INVALIDO, JUEGO_TERMINADO;
+}
